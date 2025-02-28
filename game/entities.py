080e2de1741a3f4b7b507a6cd84c08030ca91bab
Ishidawg <willianscagol@gmail.com>
2025-02-28 02:34:19 -0300

    change enemies behaviors, try to add a 3D modal and do some tests

--- game/entities.py
+++ game/entities.py
@@ -14,7 +14,7 @@ class Bullet(Entity):
 
     def update(self):
         from game.game_logic import game_objects
-        self.position += self.direction * 80 * time.dt
+        self.position += self.direction * 120 * time.dt
 
         hit_info = self.intersects()
 
@@ -25,7 +25,7 @@ class Bullet(Entity):
             return
 
         player = game_objects.get('player')
-        if player and distance(self.position, player.position) > 20:
+        if player and distance(self.position, player.position) > 40:
             destroy(self)
 
 def create_entities():
@@ -34,7 +34,10 @@ def create_entities():
         model="models/pistol.obj",
         scale=0.65,
         position=(0.4, -1, 1),
-        rotation=(0, -90, 0)
+        rotation=(0, -90, 0),
+        # always_on_top=True,
+        # render_queue=1,
+        # disable_depth_test=True,
     )
 
     middle_pistol = Entity(
@@ -43,6 +46,9 @@ def create_entities():
         scale=0.85,
         position=(0.005, -1.2, 0.85),
         rotation=(0, -90, 0),
-        visible=False
+        visible=False,
+        # always_on_top=True,
+        # render_queue=1,
+        # disable_depth_test=True,
     )
     return pistol, middle_pistol
--- game/player.py
+++ game/player.py
@@ -2,6 +2,9 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 def create_player():
     player = FirstPersonController(
-        position=(0, 80, 0)
+        model='models/player.obj',
+        scale=1,
+        position=(0, 80, 0),
+        collider='box',
     )
     return player
